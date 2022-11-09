d6786ac112719042dd784abe77e818d9507d1fc6
Inderjeet Singh <inder12801@gmail.com>
2022-11-09 19:37:59 +0530

    This is 26th commit

--- src/ColorModeSwitcher.jsx
+++ src/ColorModeSwitcher.jsx
@@ -1,5 +1,10 @@
 import React, { useContext } from "react";
-import { useColorMode, useColorModeValue, IconButton, HStack } from "@chakra-ui/react";
+import {
+  useColorMode,
+  useColorModeValue,
+  IconButton,
+  HStack,
+} from "@chakra-ui/react";
 import { FaMoon, FaSun } from "react-icons/fa";
 import { AppContext } from "./ContextAPI";
 
@@ -13,16 +18,16 @@ const ColorModeSwitcher = (props) => {
       variant="unstyled"
       color="dark"
       pos={"absolute"}
-      top={["17px",'35px']}
-      right={["60px",'30px']}
+      top={["17px", "25px"]}
+      right={["60px", "30px"]}
       size={"70px"}
       // bg={'yellow'}
-      p={'15px'}
+      p={"15px"}
       onClick={() => {
         toggleColorMode();
         // setTheming(!theming);
       }}
-      icon={<SwitchIcon color="gray"/>}
+      icon={<SwitchIcon color="gray" />}
       zIndex={"overlay"}
     />
   );
